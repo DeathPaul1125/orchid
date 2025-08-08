955cc781fbdf329446ffc2b30234586a8b75ee18
Pablo <civernet@gmail.com>
2025-08-07 21:23:16 -0600

    Finalizando la parte de las tareas, agregando las funcionalidades del modal en el screen de task

--- app/Orchid/Screens/TaskScreen.php
+++ app/Orchid/Screens/TaskScreen.php
@@ -3,6 +3,13 @@
 namespace App\Orchid\Screens;
 
 use Orchid\Screen\Screen;
+use Orchid\Screen\Fields\Input;
+use Orchid\Support\Facades\Layout;
+use Orchid\Screen\Actions\ModalToggle;
+use Orchid\Screen\Actions\Button;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Orchid\Screen\TD;
 
 class TaskScreen extends Screen
 {
@@ -13,7 +20,9 @@ class TaskScreen extends Screen
      */
     public function query(): iterable
     {
-        return [];
+        return [
+            'tasks' => Task::latest()->get(),
+        ];
     }
 
     /**
@@ -42,7 +51,12 @@ class TaskScreen extends Screen
      */
     public function commandBar(): iterable
     {
-        return [];
+        return [
+            ModalToggle::make('Agregar Tarea')
+                ->modal('taskModal')
+                ->method('create')
+                ->icon('plus'),
+        ];
     }
 
     /**
@@ -52,6 +66,76 @@ class TaskScreen extends Screen
      */
     public function layout(): iterable
     {
-        return [];
+        return [
+            Layout::table('tasks', [
+
+                TD::make('name'),
+
+                TD::make('Acciones')
+                    ->alignRight()
+                    ->render(function (Task $task) {
+                        return Button::make('Eliminar')
+                                ->confirm('Esta seguro de eliminar esta tarea, dicha opcion no se puede revertir.')
+                                ->method('delete', ['task' => $task->id])
+                                ->icon('trash')
+                            .
+                            Button::make('Editar')
+                                ->method('edit', ['task' => $task->id])
+                            ->icon('pencil');
+                        }),
+            ]),
+
+            Layout::modal('taskModal', Layout::rows([
+                Input::make('task.name')
+                    ->title('Nombre')
+                    ->placeholder('Nombre de la tarea')
+                    ->help('Agregamos el nombre de la tarea que necesitamos.'),
+            ]))
+                ->title('Tarea Nueva')
+                ->applyButton('Nueva Tarea'),
+        ];
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return void
+     */
+    public function create(Request $request)
+    {
+        // Validate form data, save task to database, etc.
+        $request->validate([
+            'task.name' => 'required|max:255',
+        ]);
+
+        $task = new Task();
+        $task->name = $request->input('task.name');
+        $task->save();
+    }
+
+    /**
+     * @param Task $task
+     *
+     * @return void
+     */
+    public function delete(Task $task)
+    {
+        $task->delete();
+    }
+
+    /**
+     * @param Task $task
+     *
+     * @return void
+     */
+    public function edit(Task $task)
+    {
+        return redirect()->route('platform.task')->with([
+            'open' => [
+                'taskModal' => [
+                    'task' => $task
+                ]
+            ]
+        ]);
     }
 }
